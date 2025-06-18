a2a75034635948fabce2a3e0a281ffa4eeb59533
Ronit Thummaluru <120275888+rthummaluru@users.noreply.github.com>
2025-06-17 21:55:16 -0400

    chore: clean up project structure and gitignore

--- journal-app-backend/vector_store.py
+++ journal-app-backend/vector_store.py
@@ -11,14 +11,16 @@ class VectorStore:
     
     #add a vector + orginial text
     def add(self, entry_text: str, embedding: List[float]):
-        vector = np.array([embedding]).asType("float32")
+        vector = np.array([embedding]).astype('float32')
         self.index.add(vector)
         self.entries.append(entry_text)
 
     #search for top k similar vectors
-    def search(self, query_embedding: List[float], k=5):
-        query_vector = np.array([query_embedding]).asType("float32")
-        distance, index = self.index.search(query_vector, k)
-        return [self.entries[i] for i in index[0]]
+    def search(self, query_embedding: List[float], k: int = 5):
+        query_vector = np.array([query_embedding]).astype('float32')
+        distances, indices = self.index.search(query_vector, k)
+        return [self.entries[i] for i in indices[0]]
 
+# Create a singleton instance
+store = VectorStore()
         
